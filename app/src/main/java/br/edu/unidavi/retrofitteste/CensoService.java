63485c74ea1055135e3ae10b1c80539eb3101f1c
Marcondes Maçaneiro <marcondesmacaneiro@gmail.com>
2016-11-04 08:06:11 -0200

    Ajuste nas configurações.

--- app/src/main/java/br/edu/unidavi/retrofitteste/CensoService.java
+++ app/src/main/java/br/edu/unidavi/retrofitteste/CensoService.java
@@ -8,17 +8,14 @@ import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
 
-/**
- * Created by shelajev on 16/12/15.
- */
 interface CensoService {
     @GET("/api/censo/{id}")
     Call<List<Coletor>> repoCenso(
             @Path("id") String repo
     );
 
-
-    public static final Retrofit retrofit = new Retrofit.Builder()
+    public static final Retrofit retrofit =
+            new Retrofit.Builder()
             .baseUrl("https://pure-island-41431.herokuapp.com/")
             .addConverterFactory(GsonConverterFactory.create())
             .build();
--- app/src/main/java/br/edu/unidavi/retrofitteste/Coletor.java
+++ app/src/main/java/br/edu/unidavi/retrofitteste/Coletor.java
@@ -1,8 +1,5 @@
 package br.edu.unidavi.retrofitteste;
 
-/**
- * Created by shelajev on 16/12/15.
- */
 public class Coletor {
 
     String coletor;
